c0d75d2537d4a85efefa1d1852778ced28f3c392
Sannamarijtje <sannaplomp@gmail.com>
2025-05-01 12:46:24 +0200

    histogram

--- maincode.py
+++ maincode.py
@@ -1,5 +1,27 @@
 import numpy
 import pandas
-import matplotlib
+import matplotlib.pyplot as plt
 import sklearn
 import nltk
+
+df_attributes = pandas.read_csv('DATA/attributes.csv')
+df_products = pandas.read_csv('DATA/product_descriptions.csv')
+df_train = pandas.read_csv('DATA/train.csv', encoding='latin1')
+df_test = pandas.read_csv('DATA/test.csv', encoding='latin1')
+
+# print(df_train.info())
+# print(df_train.shape)
+# print(len(df_train['product_title'].unique()))
+# print(df_train['product_title'].value_counts().head(10))
+print(df_train['relevance'].mean(), df_train['relevance'].median(), df_train['relevance'].std())
+
+# plt.boxplot(df_train['relevance'], vert=False)
+# plt.title('Boxplot of Relevance Values')
+# plt.xlabel('Relevance')
+# plt.show()
+
+plt.hist(df_train['relevance'], bins=50, color='blue', alpha=0.7)
+plt.title('Histogram of Relevance Values')
+plt.xlabel('Relevance')
+plt.ylabel('Frequency')
+plt.show()
