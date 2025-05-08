3f9fe7a13a8998815a925b6e4e71ee9e64443280
Sannamarijtje <sannaplomp@gmail.com>
2025-05-08 12:11:12 +0200

    explor klaar

--- maincode.py
+++ maincode.py
@@ -15,12 +15,14 @@ df_test = pandas.read_csv('DATA/test.csv', encoding='latin1')
 # print(df_train['product_title'].value_counts().head(10))
 # print(df_train['relevance'].mean(), df_train['relevance'].median(), df_train['relevance'].std())
 
-
-
 # plt.hist(df_train['relevance'], bins=50, color='blue', alpha=0.7)
 # plt.title('Histogram of Relevance Values')
 # plt.xlabel('Relevance')
 # plt.ylabel('Frequency')
 # plt.show()
 
-print(df_attributes['product_uid'].value_counts().head(5))
+# print(df_attributes['product_uid'].value_counts().head(5))
+
+filtered_df = df_attributes[df_attributes["name"].str.contains('Brand Name', na=False)]
+print(filtered_df["value"].unique())
+print(filtered_df["value"].value_counts().head(10))
